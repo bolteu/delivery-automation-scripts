cb6cb8aac390b06ab88d9b68c5973e27afb4518b
krzysztof-witkowski <krz.wit@outlook.com>
2021-05-24 15:33:18 +0200

    Updated link to login page in admin-panel
    Updated link to login page in admin-panel
    
    Test commit
    
    Deleted links from config.example.py
    
    Deleted unnecessary comments and commands

--- main.py
+++ main.py
@@ -23,19 +23,13 @@ couriers_not_registered = []
 for index, row in df.iterrows():
     try:
         driver.implicitly_wait(3)
-        #sleep(2)
         # partner profile:
         driver.execute_script("window.open('" + base_admin_panel_url + "/fleet-root/partners/new')")
-        # courier profile admin panel:
-        # driver.execute_script("window.open('" + base_admin_panel_url + "/delivery/partners/1627816/couriers/create')")
-        # driver.implicitly_wait(3)
         driver.switch_to.window(driver.window_handles[index + 1])
         url = driver.current_url
-        print(url)
         # create partner profile:
         for column in columns:
             if "partner" in column:
-                # If you want to debug - you can use next trick
                 #input(str(column) + ":" + str(get_cell_value(row, column)))
                 field_name = column.replace("partner_", "")
                 admin_panel.set_form_value(field_name, get_cell_value(row, column))
@@ -43,17 +37,10 @@ for index, row in df.iterrows():
         sleep(1)
         driver.implicitly_wait(10)
 
-
         url = driver.current_url
-        print(url)
         pattern = 'edit/(\d+)'
         id = re.search(pattern, url)[1]
-        print("ID:")
-        print(id)
         partner_url = base_admin_panel_url + "/delivery/delivery/partners/" + str(id) + "/couriers/create"
-        print("URL:")
-        print(partner_url)
-        # input(str(column) + ":" + str(get_cell_value(row, column)))
         driver.get(partner_url)
 
         # driver.find_element_by_link_text("Create").click()
@@ -74,9 +61,3 @@ if not couriers_not_registered:
 else:
     print("Phone numbers of not registered couriers:")
     print(couriers_not_registered)
-
-    # except Exception as e:
-    #     couriers_not_registered.append(get_cell_value(row, "partner_phone"))
-    #     # print("Courier not registered: ")
-    #     # print(e)
-    #     # input("Press enter to continue")
