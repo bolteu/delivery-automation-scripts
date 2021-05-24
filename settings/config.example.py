c26df02311c775727a0818633f7ce6069ce366f5
krzysztof-witkowski <krz.wit@outlook.com>
2021-05-24 16:32:03 +0200

    Deleted links from config.example.py

--- settings/config.example.py
+++ settings/config.example.py
@@ -4,5 +4,5 @@ chromedriver = "./bin/chromedriver"
 username = "bolt.xxx.yyy"
 password = "123412341234"
 
-base_admin_panel_url = 'https://admin-panel.bolt.eu'
-old_base_admin_panel_url = 'https://delivery-admin.taxify.eu'
+base_admin_panel_url = ''
+old_base_admin_panel_url = ''
